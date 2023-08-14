95536a9643f8afa15a52ce8b46612dfcdb0c1bac
Anthony Aviles <avilesanthony0617@gmail.com>
2023-08-14 11:50:24 -0500

    adding kafka package

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const { Kafka } = require("kafkajs");
 const kafka = new Kafka({
   clientId: "orders",
   brokers: ["pkc-n98pk.us-west-2.aws.confluent.cloud:9092"],
+  ssl: true,
+  sasl: {
+    mechanism: "plain",
+    username: "TVRLE5EIYD73XOGY",
+    password:
+      "/vrQg9tS263AQG+jd3SSXIRnbeEGJFYTOREE3PA2mj8VKwJDk9yBiqqVlzPB6jIr",
+  },
 });
 
 const consumer = kafka.consumer({ groupId: "order-consumption" });
