e915bf4ca3fee7319ae8d5589ea6e61b7cfecef7
Anthony Aviles <avilesanthony0617@gmail.com>
2023-08-16 09:29:19 -0500

    adding dynamodb

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
-exports.handler = async (event) => {
-  // Iterate through keys
+import { config, DynamoDb } from "aws-sdk";
+import Promise from "bluebird";
+
+config.setPromisesDependency(Promise);
+
+const updateDynamoDb = async function (event) {
+  config.update({ region: us - east - 1 });
+  let response = {
+    statusCode: 200,
+    body: JSON.stringify("Success"),
+  };
+
   for (let key in event.records) {
     console.log("Key: ", key);
     // Iterate through records
-    event.records[key].map((record) => {
+    event.records[key].map(async (record) => {
       console.log("Record: ", record);
-      // Decode base64
-      const msg = Buffer.from(record.value, "base64").toString();
-      console.log("Message:", msg);
+      const payload = record.body;
+      const params = {
+        Item: {
+          message: Buffer.from(record.value, "base64").toString(),
+        },
+        TableName: "figure it out",
+      };
+      console.log(params);
+      const documentClient = new DynamoDb.documentClient();
+      try {
+        const data = await documentClient.put(params).promise();
+        console.log(`Database successfully updated. Returned ${data}`);
+      } catch (err) {
+        if (err) {
+          console.log(err, err.stack);
+          response = {
+            statusCode: 500,
+            body: "Error occurred while updating database",
+          };
+        }
+      }
     });
   }
+  return response;
+};
+
+exports.handler = async (event) => {
+  console.log("Received event:", JSON.stringify(event, null, 2));
+  return updateDynamoDb(event);
 };
